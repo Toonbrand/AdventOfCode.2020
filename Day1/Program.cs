2be2a0b8323be0fd320d725e93d41f4e26ea80d8
toonbrand <t.brand@verdel.nl>
2020-12-01 14:16:25 +0100

    Initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1_ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"Input.txt");
+            int[] nrs = Array.ConvertAll(lines, int.Parse);
+
+            foreach (int nr in nrs)
+            {
+                int res = nrs.Where(n => n + nr == 2020).FirstOrDefault();
+                if (res != 0)
+                {
+                    Console.WriteLine(nr + " * " + res + " = " + (nr * res));
+                }
+            }
+        }
+    }
+}
