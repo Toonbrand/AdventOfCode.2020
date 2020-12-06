7de0dad85c128ffd20f775b9c3547d4df4af54c5
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-06 13:17:04 +0100

    Day 6 part 1

---
+++ AdventOfCode.2020.NET/Day6_CustomCustoms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Day1_ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string[] lines = File.ReadAllLines(@"Input.txt");
+
+            string answers = "";
+            int answerCount =0;
+            foreach (string line in lines)
+            {
+                if (line.Equals(""))
+                {
+                    answerCount += answers.Distinct().Count();
+                    answers = "";
+                }
+                else
+                {
+                    answers += line;
+                }
+            }
+
+
+            Console.WriteLine("Answers: "+answerCount);
+            
+            stopwatch.Stop();
+            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
