3bfe387f95374ed1ce80ec407d8cf588629234b5
toonbrand <t.brand@verdel.nl>
2020-12-07 17:06:01 +0100

    Started day 7 part 1

---
+++ AdventOfCode.2020.NET/Day7_HandyHaversacks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Day1_ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string[] lines = File.ReadAllLines(@"Input.txt");
+
+
+
+            stopwatch.Stop();
+            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+
+    public partial class Bag{
+        public string color { get; set; }
+        public Dictionary<Bag, int> bagRules { get; set; }
+    }
+
+}
