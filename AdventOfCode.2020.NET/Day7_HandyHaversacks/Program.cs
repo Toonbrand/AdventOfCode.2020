025be5018483b21346b0fa675044e7797e654f8e
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-07 19:51:27 +0100

    Created list of Bags of Bags

--- AdventOfCode.2020.NET/Day7_HandyHaversacks/Program.cs
+++ AdventOfCode.2020.NET/Day7_HandyHaversacks/Program.cs
@@ -13,7 +13,45 @@ namespace Day1_ReportRepair
             Stopwatch stopwatch = Stopwatch.StartNew();
             string[] lines = File.ReadAllLines(@"Input.txt");
 
+            List<Bag> bags = new List<Bag>();
 
+            foreach (string line in lines)
+            {
+                string bagColor = line.Substring(0, line.IndexOf(" bag"));
+                List<string> rules = line.Substring(line.IndexOf("contain ")+"contain ".Length).Split(", ").ToList();
+
+                Boolean newBag = false;
+                Bag bag = bags.Where(b => b.color == bagColor).FirstOrDefault();
+
+                if (bag == null)
+                {
+                    bag = new Bag();
+                    bag.color = bagColor;
+                    newBag = true;
+                }
+
+                bag.bagRules = new List<Bag>();
+                foreach (string rule in rules)
+                {
+                    if (rule.Equals("no other bags.")) continue;
+                    string ruleColor = rule.Substring(2, rule.IndexOf(" bag") - 2);
+                    int ruleInt = Int32.Parse(rule.Substring(0, 1));
+                    Bag ruleBag = bags.Where(b => b.color == ruleColor).FirstOrDefault();
+                    if (ruleBag == null)
+                    {
+                        ruleBag = new Bag() { color = ruleColor };
+                        bags.Add(ruleBag);
+                    }
+                    bag.bagRules.Add(ruleBag);
+                }
+
+                if (newBag)
+                {
+                    bags.Add(bag);
+                }
+            }
+
+            bags = bags.OrderBy(b => b.color).ToList();
 
             stopwatch.Stop();
             Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
@@ -22,7 +60,7 @@ namespace Day1_ReportRepair
 
     public partial class Bag{
         public string color { get; set; }
-        public Dictionary<Bag, int> bagRules { get; set; }
+        public List<Bag> bagRules { get; set; }
     }
 
 }
