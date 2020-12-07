8286b99312c417b31f65bc6cdc127235d518ab3e
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-07 20:36:35 +0100

    Day 7 part 1 & 2 solved

--- AdventOfCode.2020.NET/Day7_HandyHaversacks/Program.cs
+++ AdventOfCode.2020.NET/Day7_HandyHaversacks/Program.cs
@@ -8,59 +8,87 @@ namespace Day1_ReportRepair
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             string[] lines = File.ReadAllLines(@"Input.txt");
 
             List<Bag> bags = new List<Bag>();
-
             foreach (string line in lines)
             {
                 string bagColor = line.Substring(0, line.IndexOf(" bag"));
-                List<string> rules = line.Substring(line.IndexOf("contain ")+"contain ".Length).Split(", ").ToList();
+                List<string> rules = line[(line.IndexOf("contain ") + "contain ".Length)..].Split(", ").ToList();
 
                 Boolean newBag = false;
                 Bag bag = bags.Where(b => b.color == bagColor).FirstOrDefault();
 
                 if (bag == null)
                 {
-                    bag = new Bag();
-                    bag.color = bagColor;
+                    bag = new Bag{color = bagColor};
                     newBag = true;
                 }
 
-                bag.bagRules = new List<Bag>();
+                bag.bagChildren = new List<Bag>();
                 foreach (string rule in rules)
                 {
                     if (rule.Equals("no other bags.")) continue;
-                    string ruleColor = rule.Substring(2, rule.IndexOf(" bag") - 2);
-                    int ruleInt = Int32.Parse(rule.Substring(0, 1));
+                    string ruleColor = rule[2..rule.IndexOf(" bag")];
                     Bag ruleBag = bags.Where(b => b.color == ruleColor).FirstOrDefault();
+
                     if (ruleBag == null)
                     {
                         ruleBag = new Bag() { color = ruleColor };
                         bags.Add(ruleBag);
                     }
-                    bag.bagRules.Add(ruleBag);
+
+                    for (int i = 0; i < Int32.Parse(rule.Substring(0, 1)); i++)
+                    {
+                        bag.bagChildren.Add(ruleBag);
+                    }
                 }
 
-                if (newBag)
-                {
-                    bags.Add(bag);
-                }
+                if (newBag) bags.Add(bag);
             }
 
-            bags = bags.OrderBy(b => b.color).ToList();
+            Bag mybag = bags.Where(b => b.color == "shiny gold").FirstOrDefault();
+            int part2Answ = totalBagCount(mybag.bagChildren);
+
+            int part1Answ = 0;
+            foreach (Bag bag in bags) part1Answ += goldenBagCount(bag.bagChildren);
+
+            Console.WriteLine("Part 1: " + part1Answ);
+            Console.WriteLine("Part 2: " + part2Answ);
 
             stopwatch.Stop();
             Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+
+        public static int totalBagCount(IEnumerable<Bag> bags)
+        {
+            int ret = bags.Count();
+            foreach (Bag bag in bags)
+            {
+                ret += totalBagCount(bag.bagChildren);
+            }
+            return ret;
+        }
+
+        public static int goldenBagCount(IEnumerable<Bag> bags)
+        {
+            bags = bags.Distinct();
+            int res = bags.Where(b => b.color == "shiny gold").Count();
+
+            foreach (Bag bag in bags)
+            {
+                if (res > 0) return res;
+                res += goldenBagCount(bag.bagChildren);
+            }
+            return res;
         }
     }
 
     public partial class Bag{
         public string color { get; set; }
-        public List<Bag> bagRules { get; set; }
+        public List<Bag> bagChildren { get; set; }
     }
-
 }
