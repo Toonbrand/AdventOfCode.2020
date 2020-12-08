2f91434f342f94510cf797705522c05d8688f181
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-08 21:29:05 +0100

    Slight improvement

--- AdventOfCode.2020.Java/src/main/Day8.java
+++ AdventOfCode.2020.Java/src/main/Day8.java
@@ -15,24 +15,23 @@ public class Day8 {
 		String[] linesArr = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
 
 		tryRun(linesArr);
-		System.out.println("acc when ran until stuck in loop: " + acc);
+		System.out.println("[part 1] acc when ran until stuck in loop: " + acc);
 
-		for (int i = 0; i < linesArr.length; i++) {
-			acc = 0;
-			String lineComm = linesArr[i];
-			if ((linesArr[i] = lineComm.replace("nop", "jmp")) != lineComm) {
+		for (int i = 0; i < linesArr.length; i++, acc = 0) {
+			String line = linesArr[i];
+
+			if ((linesArr[i] = line.replace("nop", "jmp")) != line) {
 				if (tryRun(linesArr))
 					break;
-				linesArr[i] = lineComm.replace("jmp", "nop");
 			}
-			if ((linesArr[i] = lineComm.replace("jmp", "nop")) != lineComm) {
+			if ((linesArr[i] = line.replace("jmp", "nop")) != line) {
 				if (tryRun(linesArr))
 					break;
-				linesArr[i] = lineComm.replace("nop", "jmp");
 			}
+			linesArr[i] = line;
 		}
 
-		System.out.println("acc when ran until end: " + acc);
+		System.out.println("[part 2] acc when ran until end: " + acc);
 
 		long stopTime = System.currentTimeMillis();
 		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
@@ -48,9 +47,9 @@ public class Day8 {
 
 			String line = linesArr[lineNr];
 			passedLines.add(lineNr);
-			String lineComm = line.substring(0, line.indexOf(' '));
+			String lineComm = line.substring(0, 3);
 
-			int lineInt = Integer.parseInt(line.substring(line.indexOf(' ') + 1, line.length()));
+			int lineInt = Integer.parseInt(line.substring(4, line.length()));
 
 			switch (lineComm) {
 			case "jmp":
