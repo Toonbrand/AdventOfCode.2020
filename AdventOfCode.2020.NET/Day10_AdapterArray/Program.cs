09c0fed083482387e6b03f3974a8f06e7b793ffc
toonbrand <t.brand@verdel.nl>
2020-12-10 17:11:30 +0100

    Day 10 part 1

---
+++ AdventOfCode.2020.NET/Day10_AdapterArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day10_AdapterArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string[] lines = File.ReadAllLines(@"Input.txt");
+            int[] adapters = Array.ConvertAll(lines, l => int.Parse(l)).OrderBy(l=>l).ToArray();
+
+            int jltDiff1 = 1, jltDiff2 = 0, jltDiff3 = 1;
+            for (int i = 0; i < adapters.Length-1; i++)
+            {
+                int jmp = adapters[i + 1] - adapters[i];
+                switch (jmp)
+                {
+                    case 1:
+                        jltDiff1++;
+                        break;
+                    case 2:
+                        jltDiff2++;
+                        break;
+                    case 3:
+                        jltDiff3++;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Jolt difference 1: " + jltDiff1);
+            Console.WriteLine("Jolt difference 2: " + jltDiff2);
+            Console.WriteLine("Jolt difference 3: " + jltDiff3);
+
+            stopwatch.Stop();
+            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
