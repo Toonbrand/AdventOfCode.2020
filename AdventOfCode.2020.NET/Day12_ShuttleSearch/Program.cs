6498b2d4b22d791c3ec1f3a881ead2f65f7b11c0
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-13 19:42:41 +0100

    Day 13 part 2 unoptimized

--- AdventOfCode.2020.NET/Day12_ShuttleSearch/Program.cs
+++ AdventOfCode.2020.NET/Day12_ShuttleSearch/Program.cs
@@ -12,29 +12,64 @@ namespace Day12_ShuttleSearch
 
             Stopwatch stopwatch = Stopwatch.StartNew();
             string[] lines = File.ReadAllLines(@"Input.txt");
-            string[] busses = lines[1].Split(',').Where(s => s != "x").ToArray();
 
-            int earliestTime = Int32.Parse(lines[0]);
+            //string[] idBusses = lines[1].Split(',').Where(s => s != "x").ToArray();
+            //Console.WriteLine("Part 1: " + part1(idBusses, Int32.Parse(lines[0])));
 
-            int[] busIds = Array.ConvertAll(busses, s=> int.Parse(s));
+            int[] allBusses = lines[1].Replace('x', '1').Split(',').ToList().ConvertAll(b => int.Parse(b)).ToArray();
+
+            Boolean loop = true;
+            for (int i = 1; loop; i++)
+            {
+                long t = allBusses[0] * i;
+                long prev = t;
+                Boolean success = true;
+                foreach (int bus in allBusses.Skip(1))
+                {
+                    if((prev - (prev % bus) + bus) == prev + 1)
+                    {
+                        prev++;
+                        continue;
+                    }
+                    else
+                    {
+                        success = false;
+                        prev++;
+                        break;
+                    }
+                }
+                if (success)
+                {
+                    loop = false;
+                    Console.WriteLine(t);
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+
+            Console.WriteLine("\npress any key to exit the process...");
+            Console.ReadKey();
+        }
+
+        static int part1(string[] busses, int earliestTime)
+        {
+            int[] busIds = Array.ConvertAll(busses, s => int.Parse(s));
 
             int idealId = int.MaxValue;
             int idealTime = int.MaxValue;
 
             foreach (int busId in busIds)
             {
-                int puTime = earliestTime-(earliestTime % busId)+busId;
-                if (puTime<idealTime)
+                int puTime = earliestTime - (earliestTime % busId) + busId;
+                if (puTime < idealTime)
                 {
                     idealTime = puTime;
                     idealId = busId;
                 }
             }
 
-            Console.WriteLine((idealTime-earliestTime) *idealId);
-
-            stopwatch.Stop();
-            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+            return ((idealTime - earliestTime) * idealId);
         }
     }
 }
