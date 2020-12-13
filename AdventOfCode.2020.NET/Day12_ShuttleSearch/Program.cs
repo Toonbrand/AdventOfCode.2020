a2c6f6b59bdba8b425ce7baca1d5b680a1fa04b6
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-13 18:14:05 +0100

    Day 12 part 1

---
+++ AdventOfCode.2020.NET/Day12_ShuttleSearch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day12_ShuttleSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string[] lines = File.ReadAllLines(@"Input.txt");
+            string[] busses = lines[1].Split(',').Where(s => s != "x").ToArray();
+
+            int earliestTime = Int32.Parse(lines[0]);
+
+            int[] busIds = Array.ConvertAll(busses, s=> int.Parse(s));
+
+            int idealId = int.MaxValue;
+            int idealTime = int.MaxValue;
+
+            foreach (int busId in busIds)
+            {
+                int puTime = earliestTime-(earliestTime % busId)+busId;
+                if (puTime<idealTime)
+                {
+                    idealTime = puTime;
+                    idealId = busId;
+                }
+            }
+
+            Console.WriteLine((idealTime-earliestTime) *idealId);
+
+            stopwatch.Stop();
+            Console.WriteLine("Executed in: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
