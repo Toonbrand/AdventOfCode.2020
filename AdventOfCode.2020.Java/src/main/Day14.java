c7ac30757c27912600b275f227b9dc7819f408fd
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-15 00:01:04 +0100

    Day 14 part 2

--- AdventOfCode.2020.Java/src/main/Day14.java
+++ AdventOfCode.2020.Java/src/main/Day14.java
@@ -1,24 +1,26 @@
 package main;
 
 import java.io.File;
-import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Day14 {
 
+	static String emptyBin = "000000000000000000000000000000000000";
+	
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		File file = new File("src/main/day14_input.txt");
 		String[] lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
-
-		Map<Integer, Long> addrValues = new HashMap<Integer, Long>();
-		String emptyBin = "000000000000000000000000000000000000";
-		String mask = "";
 		
+		Map<Long, Long> addrValues = new HashMap<Long, Long>();
+		
+		String mask = "";
 		for (String line : lines) {
 			if (line.length() == 43) {
 				mask = line.substring(7);
@@ -26,26 +28,54 @@ public class Day14 {
 			else {
 				int mem = Integer.parseInt(line.substring(line.indexOf("[")+1, line.indexOf("]")));
 				long val = Integer.parseInt(line.substring(line.indexOf("=")+2)); 
-				String binValStr = emptyBin.concat(Long.toBinaryString(val));
+				String binValStr = emptyBin.concat(Long.toBinaryString(mem));
 				char[] binValArr = binValStr.substring(binValStr.length()-36).toCharArray();
 				
+				int floaters = 0;
 				for (int i = 0; i < binValArr.length; i++) {
 					char repl = mask.charAt(i);
-					if(repl == 'X'){
+					if(repl == '0'){
 						continue;
 					}
 					else {
 						binValArr[i]=repl;
+						if(repl == 'X'){
+							floaters++;
+						}
 					}
 				}
+				
+				List<String> keys = new ArrayList<String>();
+				for (int i=0; i<Math.pow(2, floaters); i++) {
+					int num = Integer.parseInt(Integer.toBinaryString(i));
+					keys.add(String.format("%0"+floaters+"d", num));
+				}
+				
+				List<String> binStrLst = new ArrayList<String>();
+				for (int i = 0; i < keys.size(); i++) {
+					char[] newBin = Arrays.copyOf(binValArr, binValArr.length);
+					char[] key = keys.get(i).toCharArray();
 
-				long newVal = Long.parseLong(new String(binValArr), 2);
-				addrValues.put(mem, newVal);
+					for (int j=0, k=0;j<binValArr.length;j++) {
+						if (binValArr[j] == 'X') {
+							newBin[j] = key[k];
+							k++;
+						}
+					}
+					
+					String newBinStr = new String(newBin);
+					binStrLst.add(newBinStr);
+				}
+
+				for (String string : binStrLst) {
+					long floatVal = Long.parseLong(string, 2);
+					addrValues.put(floatVal, val);
+				}
 			}
 		}
 		
 		long tot = 0;
-		for (Map.Entry<Integer, Long> entry : addrValues.entrySet()) {
+		for (Map.Entry<Long, Long> entry : addrValues.entrySet()) {
 			tot += entry.getValue();
 		}
 		
@@ -54,5 +84,4 @@ public class Day14 {
 		long stopTime = System.currentTimeMillis();
 		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
 	}
-
 }
