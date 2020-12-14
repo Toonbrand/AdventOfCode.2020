53d2e9cd486e686b9d7194a8f36bda3b5b639933
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-15 00:07:41 +0100

    Day 14 combined part 1 & 2

--- AdventOfCode.2020.Java/src/main/Day14.java
+++ AdventOfCode.2020.Java/src/main/Day14.java
@@ -12,14 +12,21 @@ import java.util.Map;
 public class Day14 {
 
 	static String emptyBin = "000000000000000000000000000000000000";
-	
+
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		File file = new File("src/main/day14_input.txt");
 		String[] lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
-		
-		Map<Long, Long> addrValues = new HashMap<Long, Long>();
-		
+
+//		System.out.println("[part 1]: " + part1(lines));
+		System.out.println("[part 2]: " + part2(lines));
+
+		long stopTime = System.currentTimeMillis();
+		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
+	}
+	
+	public static long part1(String[] lines){
+		Map<Integer, Long> addrValues = new HashMap<Integer, Long>();
 		String mask = "";
 		for (String line : lines) {
 			if (line.length() == 43) {
@@ -28,41 +35,75 @@ public class Day14 {
 			else {
 				int mem = Integer.parseInt(line.substring(line.indexOf("[")+1, line.indexOf("]")));
 				long val = Integer.parseInt(line.substring(line.indexOf("=")+2)); 
-				String binValStr = emptyBin.concat(Long.toBinaryString(mem));
+				String binValStr = emptyBin.concat(Long.toBinaryString(val));
 				char[] binValArr = binValStr.substring(binValStr.length()-36).toCharArray();
 				
-				int floaters = 0;
 				for (int i = 0; i < binValArr.length; i++) {
 					char repl = mask.charAt(i);
-					if(repl == '0'){
+					if(repl == 'X'){
 						continue;
 					}
 					else {
 						binValArr[i]=repl;
-						if(repl == 'X'){
+					}
+				}
+
+				long newVal = Long.parseLong(new String(binValArr), 2);
+				addrValues.put(mem, newVal);
+			}
+		}
+		
+		long tot = 0;
+		for (Map.Entry<Integer, Long> entry : addrValues.entrySet()) {
+			tot += entry.getValue();
+		}
+		
+		return tot;
+	}
+
+	public static long part2(String[] lines) {
+		Map<Long, Long> addrValues = new HashMap<Long, Long>();
+		String mask = "";
+		for (String line : lines) {
+			if (line.length() == 43) {
+				mask = line.substring(7);
+			} else {
+				int mem = Integer.parseInt(line.substring(line.indexOf("[") + 1, line.indexOf("]")));
+				long val = Integer.parseInt(line.substring(line.indexOf("=") + 2));
+				String binValStr = emptyBin.concat(Long.toBinaryString(mem));
+				char[] binValArr = binValStr.substring(binValStr.length() - 36).toCharArray();
+
+				int floaters = 0;
+				for (int i = 0; i < binValArr.length; i++) {
+					char repl = mask.charAt(i);
+					if (repl == '0') {
+						continue;
+					} else {
+						binValArr[i] = repl;
+						if (repl == 'X') {
 							floaters++;
 						}
 					}
 				}
-				
+
 				List<String> keys = new ArrayList<String>();
-				for (int i=0; i<Math.pow(2, floaters); i++) {
+				for (int i = 0; i < Math.pow(2, floaters); i++) {
 					int num = Integer.parseInt(Integer.toBinaryString(i));
-					keys.add(String.format("%0"+floaters+"d", num));
+					keys.add(String.format("%0" + floaters + "d", num));
 				}
-				
+
 				List<String> binStrLst = new ArrayList<String>();
 				for (int i = 0; i < keys.size(); i++) {
 					char[] newBin = Arrays.copyOf(binValArr, binValArr.length);
 					char[] key = keys.get(i).toCharArray();
 
-					for (int j=0, k=0;j<binValArr.length;j++) {
+					for (int j = 0, k = 0; j < binValArr.length; j++) {
 						if (binValArr[j] == 'X') {
 							newBin[j] = key[k];
 							k++;
 						}
 					}
-					
+
 					String newBinStr = new String(newBin);
 					binStrLst.add(newBinStr);
 				}
@@ -73,15 +114,12 @@ public class Day14 {
 				}
 			}
 		}
-		
+
 		long tot = 0;
 		for (Map.Entry<Long, Long> entry : addrValues.entrySet()) {
 			tot += entry.getValue();
 		}
-		
-		System.out.println(tot);
 
-		long stopTime = System.currentTimeMillis();
-		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
+		return tot;
 	}
 }
