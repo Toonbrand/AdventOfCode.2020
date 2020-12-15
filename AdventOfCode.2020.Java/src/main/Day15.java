10ed1671dcc80b5d7c4f4afb9e833d6018652c22
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-15 22:06:35 +0100

    Day 15 part 1 & 2

--- AdventOfCode.2020.Java/src/main/Day15.java
+++ AdventOfCode.2020.Java/src/main/Day15.java
@@ -5,41 +5,47 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Day15 {
 
-	static String emptyBin = "000000000000000000000000000000000000";
-
+	static String[] nrsArr;
+	
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		File file = new File("src/main/day15_input.txt");
 		String[] lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
-		String[] nrsArr = lines[0].split(",");
-		int turns = 30000000;
+		nrsArr = lines[0].split(",");
 		
-		List<Integer> nrs = new ArrayList<Integer>();
-		for (String str : nrsArr) {
-			nrs.add(Integer.parseInt(str));
-		}
-		
-		for (int i = nrs.size()-1; i < turns-1; i++) {
-			List<Integer> nrsPrev = new ArrayList<Integer>(nrs);
-			nrsPrev.remove(nrs.size()-1);
-			int curr = nrs.get(i);
-			if (!nrsPrev.contains(curr)) {
-				nrs.add(0);
-			}
-			else {
-				nrs.add(nrs.lastIndexOf(curr)-nrsPrev.lastIndexOf(curr));
-			}
-		}
-		
-		System.out.println("[Part 1] The " +turns+"th number spoken is " + nrs.get(nrs.size()-1));
+		System.out.println("[Part 1] The 2020th number spoken is " + getNthTurnNr(2020));
+		System.out.println("[Part 2] The 30000000th number spoken is " + getNthTurnNr(30000000));
 		
 		long stopTime = System.currentTimeMillis();
 		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
+	}
+	
+	public static int getNthTurnNr(int turns){
+		int next = Integer.parseInt(nrsArr[nrsArr.length-1]);
+		
+		HashMap<Integer, Integer> nrs = new HashMap<Integer, Integer>();
+		for (int i=1;i<nrsArr.length;i++) {
+			nrs.put(Integer.parseInt(nrsArr[i-1]), i);
+		}
+		
+		for (int turn = nrs.size()+1; turn < turns; turn++) {
+			if(nrs.get(next)==null){
+				nrs.put(next, turn);
+				next = 0;
+			}
+			else {
+				int diff = turn-nrs.get(next);
+				nrs.put(next, turn);
+				next = diff;
+			}
+		}		
+		return next;
 	}
 }
