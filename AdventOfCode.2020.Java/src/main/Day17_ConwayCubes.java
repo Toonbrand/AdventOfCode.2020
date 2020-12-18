e504906490a22c06e50aabfd9f7f5794a399a6cd
Toon Brand <toonbrand@hotmail.com>
2020-12-18 16:38:33 +0100

    Day 17 part 1 finished

--- AdventOfCode.2020.Java/src/main/Day17_ConwayCubes.java
+++ AdventOfCode.2020.Java/src/main/Day17_ConwayCubes.java
@@ -3,54 +3,126 @@ package main;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Day17_ConwayCubes {
 
-	static Map<Integer, Map<Integer, Map<Integer, Character>>> zMap = new HashMap<Integer, Map<Integer, Map<Integer, Character>>>();
+	static final int CYCLES = 6;
+	static int gridXLenght, gridYLenght, actCount;
+	static String[] lines;
 
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		File file = new File("src/main/day17_input.txt");
-		String[] lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
-		fillZMap(lines);
-		
-		char[] test = new char[3];
-		test[1] = '#';
-		for (char c : test) {
-			System.out.println(c=='#');
+		lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
+		gridXLenght = lines[0].length();
+		gridYLenght = lines.length;
+
+		Boolean[][][] cube = createEmptyCube();
+		cube = fillInitialCube(cube);
+
+		for (int i = 0; i < CYCLES; i++) {
+			cube = runCycle(cube);
 		}
 
-		Map<Integer, Map<Integer, Map<Integer, Character>>> newZMap = new HashMap<Integer, Map<Integer, Map<Integer, Character>>>();
-		for (int z = 0; z < zMap.size(); z++) {
-			Map<Integer, Map<Integer, Character>> newYMap = new HashMap<Integer, Map<Integer, Character>>();
-			for (int y = 0; y < zMap.get(z).size(); y++) {
-				Map<Integer, Character> newXMap = new HashMap<Integer, Character>();
-				for (int x = 0; x < zMap.get(z).get(y).size(); x++) {
-					if (zMap.get(z).get(y).get(x) == '.') {
-						System.out.print(".");
-					} else {
-						System.out.print("#");
-					}
-				}
-				System.out.println();
-			}
-		}
+		System.out.println("[Part 1] Active cubes: " + actCount);
 
 		long stopTime = System.currentTimeMillis();
 		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
 	}
 
-	public static void fillZMap(String[] lines) {
-		Map<Integer, Map<Integer, Character>> yMap = new HashMap<Integer, Map<Integer, Character>>();
-		for (int i = 0; i < lines.length; i++) {
-			Map<Integer, Character> xMap = new HashMap<Integer, Character>();
-			for (int j = 0; j < lines[i].toCharArray().length; j++) {
-				xMap.put(j, lines[i].toCharArray()[j]);
+	// Run rules for every square and return a cube with the new states
+	public static Boolean[][][] runCycle(Boolean[][][] oldCube) {
+		Boolean[][][] newCube = createEmptyCube();
+
+		actCount = 0;
+		for (int z = 1; z < oldCube.length - 1; z++) {
+			for (int y = 1; y < oldCube[z].length - 1; y++) {
+				for (int x = 1; x < oldCube[z][y].length - 1; x++) {
+					int count = countSurrSqrs(oldCube, x, y, z);
+					if (oldCube[z][y][x] == true) {
+						if (count == 2 || count == 3) {
+							newCube[z][y][x] = true;
+							actCount++;
+						} else {
+							newCube[z][y][x] = false;
+						}
+					} else {
+						if (count == 3) {
+							newCube[z][y][x] = true;
+							actCount++;
+						} else {
+							newCube[z][y][x] = false;
+						}
+					}
+				}
 			}
-			yMap.put(i, xMap);
 		}
-		zMap.put(0, yMap);
+
+		return newCube;
+	}
+
+	// Count the surrounding active squares for a position
+	public static int countSurrSqrs(Boolean[][][] cube, int x, int y, int z) {
+		int act = 0;
+		for (int relZ = -1; relZ < 2; relZ++) {
+			for (int relY = -1; relY < 2; relY++) {
+				for (int relX = -1; relX < 2; relX++) {
+					if (relX == 0 && relY == 0 && relZ == 0) {
+						continue;
+					}
+					if (cube[z - relZ][y - relY][x - relX] == true) {
+						act++;
+					}
+				}
+			}
+		}
+		return act;
+	}
+
+	// Fill the empty cube with the input
+	public static Boolean[][][] fillInitialCube(Boolean[][][] zArr) {
+		for (int i = 0; i < lines.length; i++) {
+			for (int j = 0; j < lines[i].length(); j++) {
+				if (lines[i].charAt(j) == '#') {
+					zArr[CYCLES][i + CYCLES][j + CYCLES] = true;
+				}
+			}
+		}
+		return zArr;
+	}
+
+	// Create empty cube
+	public static Boolean[][][] createEmptyCube() {
+		Boolean[][][] cube = new Boolean[1 + (CYCLES * 2)][][];
+		for (int z = 0; z < cube.length; z++) {
+			Boolean[][] yArr = new Boolean[gridYLenght + ((1 + CYCLES) * 2)][];
+			for (int y = 0; y < yArr.length; y++) {
+				Boolean[] xArr = new Boolean[gridXLenght + ((1 + CYCLES) * 2)];
+				for (int x = 0; x < xArr.length; x++) {
+					xArr[x] = false;
+				}
+				yArr[y] = xArr;
+			}
+			cube[z] = yArr;
+		}
+		return cube;
+	}
+
+	// Just for debugging
+	public static void printCube(Boolean[][][] cube) {
+		for (int z = 0; z < cube.length; z++) {
+			System.out.println("z" + z);
+			for (int y = 0; y < cube[z].length; y++) {
+
+				for (int x = 0; x < cube[z][y].length; x++) {
+					if (cube[z][y][x] == true) {
+						System.out.print(1);
+					} else {
+						System.out.print(0);
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
 }
