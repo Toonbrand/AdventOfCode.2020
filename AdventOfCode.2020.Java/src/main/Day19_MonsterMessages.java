78daf2af13f6b2bbc8f4f73376dbdb2f5a1a1da5
Toon Brand (home pc) <toonbrand@hotmail.com>
2020-12-23 22:27:13 +0100

    Day 19 Part 1 & 2

--- AdventOfCode.2020.Java/src/main/Day19_MonsterMessages.java
+++ AdventOfCode.2020.Java/src/main/Day19_MonsterMessages.java
@@ -10,87 +10,89 @@ import java.util.Map;
 
 public class Day19_MonsterMessages {
 
-	static Map<Integer, String> rules;
-	static int longestMessage = 0;
-	static List<String> recMessages;
+	public static List<String> recMessages;
 
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		File file = new File("src/main/day19_input.txt");
 		String[] lines = Files.readAllLines(file.toPath(), Charset.defaultCharset()).toArray(new String[0]);
 
-		rules = new HashMap<Integer, String>();
+		Map<Integer, String> rulesPt1 = new HashMap<Integer, String>();
 		int i = 0;
-		for (;i < lines.length; i++) {
+		for (; i < lines.length; i++) {
 			String line = lines[i];
-			if (line.length()==0)
+			if (line.length() == 0)
 				break;
 			Integer ruleNr = Integer.parseInt(line.substring(0, line.indexOf(":")));
 			String rule = line.substring(line.indexOf(":") + 2, line.length());
-			rules.put(ruleNr, rule);
+			rulesPt1.put(ruleNr, rule);
 		}
-		
+
 		recMessages = new ArrayList<String>();
 		for (i++; i < lines.length; i++) {
 			recMessages.add(lines[i]);
-			if(lines[i].length() > longestMessage){
-				longestMessage = lines[i].length();
-			}
 		}
-		
-		List<String> in = new ArrayList<String>();
-		in.add("");
-		List<String> messages = getMessages(rules.get(0).split(" "), in);
-		
-		int res = 0;
-		for (String message : messages) {
-			if(recMessages.contains(message)){
-				res++;
-				recMessages.remove(message);
-			}
-		}
-		
-		System.out.println(res);
-		
+
+		Map<Integer, String> rulesPt2 = new HashMap<Integer, String>(rulesPt1);
+		rulesPt2.put(8, "42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42");
+		rulesPt2.put(11, "42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31");
+
+		System.out.println("[Part 1]: " + countValids(rulesPt1));
+		System.out.println("[Part 2]: " + countValids(rulesPt2));
+
 		long stopTime = System.currentTimeMillis();
 		System.out.println("\nExecution time: " + (stopTime - startTime) + "ms");
 	}
 
-	public static List<String> getMessages(String[] numbers, List<String> res) {
+	public static int countValids(Map<Integer, String> rules) {
+		List<String> in = new ArrayList<String>();
+		in.add("");
+		List<String> messages = getMessages(rules.get(0).split(" "), in, rules);
+
+		int res = 0;
+		for (String message : messages) {
+			if (recMessages.contains(message)) {
+				res++;
+			}
+		}
+		return res;
+	}
+
+	public static List<String> getMessages(String[] numbers, List<String> res, Map<Integer, String> rules) {
 		for (String s : numbers) {
 			res = removeInvalids(res);
-			
+
 			int i = Integer.parseInt(s);
 			String rule = rules.get(i);
 			if (rule.contains("a")) {
 				for (int j = 0; j < res.size(); j++) {
-					res.set(j, res.get(j)+"a");
+					res.set(j, res.get(j) + "a");
 				}
 			} else if (rule.contains("b")) {
 				for (int j = 0; j < res.size(); j++) {
-					res.set(j, res.get(j)+"b");
+					res.set(j, res.get(j) + "b");
 				}
 			} else if (rule.contains("|")) {
 				String[] splt = rule.split("\\|");
 				List<String> tmp = new ArrayList<String>();
-				
+
 				for (String string : splt) {
 					String[] nmbrs = string.trim().split(" ");
-					tmp.addAll(getMessages(nmbrs, new ArrayList<>(res)));
+					tmp.addAll(getMessages(nmbrs, new ArrayList<>(res), rules));
 				}
 				res = tmp;
 			} else {
-				res = getMessages(rule.split(" "), res);
+				res = getMessages(rule.split(" "), res, rules);
 			}
 		}
 
 		return res;
 	}
-	
-	public static List<String> removeInvalids(List<String> res){
+
+	public static List<String> removeInvalids(List<String> res) {
 		for (int k = 0; k < res.size(); k++) {
-			String test = res.get(k);
-			if(!recMessages.stream().anyMatch((a) -> a.startsWith(test))){
+			String mess = res.get(k);
+			if (!recMessages.stream().anyMatch((a) -> a.startsWith(mess))) {
 				res.remove(k);
 				k--;
 			}
